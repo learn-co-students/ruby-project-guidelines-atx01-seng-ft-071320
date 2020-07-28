ba8df985fca0db2a2b8fef61a29f3a32d23b5a8e
allenorton <norton.allene@gmail.com>
2020-07-28 14:09:02 -0500

    took strings out of array for entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ activity = Journal.create(journal_name: "Activity")
 allene = User.create(user_name: "Allene")
 christina = User.create(user_name: "Christina")
 
-Entry.create(entry_text: ["I had a good morning!"], user: christina, journal: personal)
-Entry.create(entry_text: ["I did yoga today."], user: allene, journal: activity)
-Entry.create(entry_text: ["I worked on the project."], user: christina, journal: work)
-Entry.create(entry_text: ["I look forward to eating dinner."], user: allene, journal: personal)
+Entry.create(entry_text: "I had a good morning!", user: christina, journal: personal)
+Entry.create(entry_text: "I did yoga today.", user: allene, journal: activity)
+Entry.create(entry_text: "I worked on the project.", user: christina, journal: work)
+Entry.create(entry_text: "I look forward to eating dinner.", user: allene, journal: personal)
 
