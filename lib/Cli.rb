c2be17f2759358f2ce1d14e43c210fc9a29c1612
christina888 <cechianelli@gmail.com>
2020-07-28 16:26:13 -0500

    welcome page

---
+++ lib/Cli.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Cli
+
+    attr_reader :journal_user
+
+    def welcome_message
+        puts "Welcome to Emoticreate!" 
+        puts "Please tell me your name."
+        user = gets.chomp
+
+        find_name = User.find{|user| user.user_name.upcase == user.upcase}
+        @journal_user = User.find_or_create_by(name: user)
+
+        if find_name == true
+            puts "\nWelcome back #{user}! Let's take some time to reflect on our day."
+            puts "Let's do a breathing exercise."
+                sleep (2.seconds)
+            puts "Breathe in..."
+                sleep(4.seconds)
+            puts "Hold..."
+                sleep(8.seconds)
+            puts "Breathe out..."
+                sleep(6.seconds)
+        else
+            puts "\nWelcome to Emoticreate, #{user}!"
+            puts "Let's do a breathing exercise."
+                sleep (2.seconds)
+            puts "Breathe in..."
+                sleep(4.seconds)
+            puts "Hold..."
+                sleep(8.seconds)
+            puts "Breathe out..."
+                sleep(6.seconds)
+        end
+#binding.pry
+    end
+end
