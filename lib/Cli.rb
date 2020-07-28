3ded29509b51d8ee24da0c92fe68a51657f5c3a0
allenorton <norton.allene@gmail.com>
2020-07-28 16:35:24 -0500

    added gem, edited runfile, and changed user variable to user_n

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -6,15 +6,15 @@ class Cli
     def welcome_message
         puts "Welcome to Emoticreate!" 
         puts "Please tell me your name."
-        user = gets.chomp
+        user_n = gets.chomp
 
-        find_name = User.find{|user| user.user_name.upcase == user.upcase}
-        @journal_user = User.find_or_create_by(name: user)
+        find_name = User.find{|user| user.user_name.upcase == user_n.upcase}
+        @journal_user = User.find_or_create_by(name: user_n)
 
         if find_name == true
             puts "\nWelcome back #{user}! Let's take some time to reflect on our day."
             puts "Let's do a breathing exercise."
-                sleep (2.seconds)
+                sleep(2.seconds)
             puts "Breathe in..."
                 sleep(4.seconds)
             puts "Hold..."
