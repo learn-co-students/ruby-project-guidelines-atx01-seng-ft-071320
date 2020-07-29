4fd72aa4fca341ab244bd5066ee58a8f1b3a5029
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-07-29 13:19:33 -0500

    created models and migration files.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
 require_relative '../config/environment'
+require 'pry'
 
+tomas = User.find_or_create_by(username:"Tomas Username")
+sohyun = User.find_or_create_by(username:"Sohyun Username")
 
+article1 = Article.find_or_create_by(title: "Test Article_1")
+article2 = Article.find_or_create_by(title: "Test Article_2")
+binding.pry
 
 puts "HELLO WORLD"
