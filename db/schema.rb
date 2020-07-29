f99b2e8c0d6b622c144c051972585903dcf79ca5
allenorton <norton.allene@gmail.com>
2020-07-29 15:47:55 -0500

    added created on column to entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_160915) do
+ActiveRecord::Schema.define(version: 2020_07_29_204443) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "journal_id"
     t.integer "user_id"
     t.text "entry_text"
     t.string "emotion"
+    t.datetime "created_on"
   end
 
   create_table "journals", force: :cascade do |t|
