660590c5af66c6ac0f7e1b9a40755c7028cbea1b
christina888 <cechianelli@gmail.com>
2020-07-30 12:02:00 -0500

    new additions

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -4,6 +4,18 @@ class Cli
 
     attr_reader :journal_user
 
+    def ascii_welcome
+        File.readlines("./lib/ascii_welcome.txt") do |line|
+          puts line
+        end
+    end
+
+     def ascii_breathein
+        File.readlines("./lib/ascii_breathein.txt") do |line|
+          puts line
+        end
+    end
+
     def welcome_message
         puts "Welcome to Emoticreate!" 
         puts "Please tell me your name."
@@ -11,15 +23,17 @@ class Cli
 
         find_name = User.find{|user| user.name.upcase == user_n.upcase}
         @journal_user = User.find_or_create_by(name: user_n)
-
-        if find_name == true
+    
+        if find_name
+            puts ascii_welcome
+            #binding.pry
             puts "\nWelcome back #{user_n}! Let's take some time to reflect on our day."
             puts "To help with that, let's do a breathing exercise."
             2.times do 
                 sleep(2.seconds)
                 puts "Ready?"
                 sleep(1.seconds)
-                    puts "Breathe in..."
+                    puts ascii_breathein
                     sleep(4.seconds)
                     puts "Hold..."
                     sleep(8.seconds)
