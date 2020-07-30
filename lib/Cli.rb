4bdf3612305ba4ff76dbaaa3129794165aee1248
allenorton <norton.allene@gmail.com>
2020-07-30 12:14:37 -0500

    accepted changes from christina merge

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -23,7 +23,6 @@ class Cli
 
         find_name = User.find{|user| user.name.upcase == user_n.upcase}
         @journal_user = User.find_or_create_by(name: user_n)
-<<<<<<< HEAD
 
         if find_name 
             puts "\nWelcome back #{user_n}! Let's take some time to reflect on our day."
@@ -43,29 +42,6 @@ class Cli
             #         #sleep(2.seconds)
             #         puts `clear`
             @journal_user.menu
-=======
-    
-        if find_name
-            puts ascii_welcome
-            #binding.pry
-            puts "\nWelcome back #{user_n}! Let's take some time to reflect on our day."
-            puts "To help with that, let's do a breathing exercise."
-            2.times do 
-                sleep(2.seconds)
-                puts "Ready?"
-                sleep(1.seconds)
-                    puts ascii_breathein
-                    sleep(4.seconds)
-                    puts "Hold..."
-                    sleep(8.seconds)
-                    puts "Breathe out..."
-                    sleep(6.seconds)
-                    puts "Let's do it again!"
-                    sleep(2.seconds)
-                    puts `clear`
-            end
->>>>>>> 660590c5af66c6ac0f7e1b9a40755c7028cbea1b
-            
         else
             puts "\nWelcome to Emoticreate, #{user_n}! Let's take some time to reflect on our day."
             sleep(2.seconds)
